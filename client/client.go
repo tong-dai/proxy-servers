72dc6dbb63ed2450d4ee773f7fae8669a7fd1b02
EmilioC1 <ec2736@princeton.edu>
2023-12-13 00:13:26 -0500

    added basic server

--- client/client.go
+++ client/client.go
@@ -4,6 +4,7 @@ import (
 	// "net/http"
 	"bufio"
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -13,17 +14,24 @@ import (
 // }
 
 // func sendEnrollRequest(stuentID []byte, classNumber []byte) bool {
-	
-// }
 
+// }
 
 // Validate student ID and class number input
 func isValidInput(input []byte, expectedLength int) bool {
-	if (len(input) > 0) { input = input[:len(input)-1] }
+	if len(input) > 0 {
+		input = input[:len(input)-1]
+	}
 
-	if (len(input) != expectedLength) { return false }
+	if len(input) != expectedLength {
+		return false
+	}
 
-	for _, b := range input { if b > 57 || b < 48 { return false } }
+	for _, b := range input {
+		if b > 57 || b < 48 {
+			return false
+		}
+	}
 	return true
 }
 
@@ -33,16 +41,24 @@ func main() {
 
 	fmt.Println("Enter Student ID: ")
 	studentID, _ := reader.ReadBytes('\n')
-	if (!isValidInput(studentID, 9)) {
-		fmt.Println("Please enter your 9-digit student ID correctly.")
-		return
-	}
+	fmt.Println(studentID)
+	// if (!isValidInput(studentID, 9)) {
+	// 	fmt.Println("Please enter your 9-digit student ID correctly.")
+	// 	return
+	// }
 
 	fmt.Println("Enter Class Number: ")
 	classNumber, _ := reader.ReadBytes('\n')
-	if (!isValidInput(classNumber, 5)) {
-		fmt.Println("Please enter the 5-digit class number correctly.")
-		return
+	fmt.Println(classNumber)
+	response, err := http.Get("http://localhost:8080/enroll")
+	if err != nil {
+		fmt.Println(err)
 	}
+	fmt.Println(response)
+
+	// if (!isValidInput(classNumber, 5)) {
+	// 	fmt.Println("Please enter the 5-digit class number correctly.")
+	// 	return
+	// }
 
 }
--- load_balancer/load_balancer.go
+++ load_balancer/load_balancer.go
@@ -1,5 +1,39 @@
-package load_balancer
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func SendReqToCache(student int, class int) bool {
 	return true
+}
+
+func main() {
+	fmt.Println("Server running")
+	http.HandleFunc("/enroll", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		var body map[string]interface{}
+		json.NewDecoder(r.Body).Decode(body)
+		fmt.Println("received response", body)
+		var response []byte = make([]byte, 5)
+		for i := 0; i < 5; i++ {
+			response[i] = byte(i)
+		}
+		w.Write(response)
+	})
+	http.HandleFunc("/enroll/:classNumber", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Query params", r.URL.Query())
+		var response []byte = make([]byte, 10)
+		for i := 0; i < 10; i++ {
+			response[i] = byte(i)
+		}
+		w.Write(response)
+
+	})
+	http.ListenAndServe(":8080", nil)
 }
