784176fae31e2e6311b0a05eb3247be6e01282a9
EmilioC1 <ec2736@princeton.edu>
2023-12-14 18:26:26 -0500

    added stuff in database and added the enroll function

--- database/database.go
+++ database/database.go
@@ -1,33 +1,109 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"sync"
-// )
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
 
-// type ClassInfo struct {
-// 	maxEnrollment int
-// 	enrollment    int
-// 	enrolledIds   []int
-// }
+type ClassInfo struct {
+	maxEnrollment int
+	enrollment    int
+	enrolledIds   []int
+}
 
-// type DB struct {
-// 	c map[int]*ClassInfo
-// 	sync.Mutex
-// }
+type CacheClassInfo struct {
+	maxEnrollment int
+	enrollment    int
+}
+
+type DB struct {
+	c map[int]*ClassInfo
+	sync.Mutex
+}
+type Server struct {
+	serverURL string
+	index     int
+	sync.Mutex
+	classes map[int]*CacheClassInfo
+}
+
+var db *DB = &DB{c: make(map[int]*ClassInfo)}
+
 // var DB map[int]*ClassInfo = make(map[int]*ClassInfo)
 
-// func (db *DB) UpdateDB(studentID int, classNumber int, cacheIndex int) (bool, int) {
-// 	db.Lock()
-// 	defer db.Unlock()
-// 	class := db.c[classNumber]
-// 	if class.enrollment == class.maxEnrollment {
-// 		// TODO send message to all caches saying that you need to update this
-// 		fmt.Println("something went wrong")
-// 		return false, classNumber
-// 	} else {
-// 		class.enrolledIds = append(class.enrolledIds, studentID)
-// 		return true, -1
-// 	}
-	
-// }
+func DeleteClass(servers []*Server, classNumber int) {
+	for i := 0; i < len(servers); i++ {
+		servers[i].Lock()
+		_, found := servers[i].classes[classNumber]
+		if found {
+			delete(servers[i].classes, classNumber)
+		}
+		servers[i].Unlock()
+	}
+}
+
+// send the new enrollemnt to all servers except for the one that sent the last request
+func UpdateServers(servers []*Server, classNumber int, enrollment int, serverIndex int) {
+	for i := 0; i < len(servers); i++ {
+		if i != serverIndex {
+			servers[i].Lock()
+			class, found := servers[i].classes[classNumber]
+			if !found {
+				continue
+			}
+			if enrollment == class.maxEnrollment {
+				delete(servers[i].classes, classNumber)
+			} else {
+				servers[i].classes[classNumber].enrollment = enrollment
+			}
+			servers[i].Unlock()
+		}
+	}
+}
+
+func UpdateDB(studentID int, classNumber int, servers []*Server, serverIndex int) bool {
+	db.Lock()
+	defer db.Unlock()
+	class := db.c[classNumber]
+	if class.enrollment == class.maxEnrollment {
+		defer DeleteClass(servers, classNumber)
+		fmt.Printf("something went wrong enrolling student %v in class %v", studentID, classNumber)
+		return false
+	} else {
+		//TODO maybe add a delay here to imitate accessing an actual database?
+		class.enrolledIds = append(class.enrolledIds, studentID)
+		// TODO figure out when to call this method => how often?
+		defer UpdateServers(servers, classNumber, class.enrollment, serverIndex)
+		return true
+	}
+
+}
+
+func Enroll(servers []*Server, i int, w http.ResponseWriter, r *http.Request) {
+	studentID, err := strconv.Atoi(r.URL.Query().Get("studentID"))
+	if err != nil {
+		log.Panic("something went wrong converting studentID to an int")
+	}
+	classNumber, err := strconv.Atoi(r.URL.Query().Get("classNumber"))
+	if err != nil {
+		log.Panic("something went converting classNumber to an int")
+	}
+	servers[i].Lock()
+	defer servers[i].Unlock()
+	class, found := servers[i].classes[classNumber]
+	if found {
+		fmt.Println("found the class")
+		class.enrollment++
+		success := UpdateDB(studentID, classNumber, servers, i)
+		if success {
+			fmt.Fprintf(w, "Successfully enrolled in %v", classNumber)
+		} else {
+			fmt.Fprintf(w, "Sorry, you were not enrolled in class %v", classNumber)
+		}
+	} else {
+		fmt.Fprintf(w, "Sorry, you were not actually enrolled in class %v", classNumber)
+	}
+}
--- proxy/main.go
+++ proxy/main.go
@@ -1,75 +1,103 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+    db "cos316/cd_ec_final_project/database"
 )
 
-
 type EnrollmentInfo struct {
-    StudentID   string
-    ClassNum    string
+	StudentID string
+	ClassNum  string
 }
 
 func processQuery(r *http.Request) *EnrollmentInfo {
-    studentID := r.URL.Query().Get("studentID")
-    classNum := r.URL.Query().Get("classNum")
-    return &EnrollmentInfo{StudentID: studentID, ClassNum: classNum}
+	studentID := r.URL.Query().Get("studentID")
+	classNum := r.URL.Query().Get("classNum")
+	return &EnrollmentInfo{StudentID: studentID, ClassNum: classNum}
 }
 
 func main() {
 
-    // Create Main Server
-    fmt.Println("Server started on: http://localhost:9000")
-    main_server := http.NewServeMux()
+	// Create Main Server
+	fmt.Println("Server started on: http://localhost:9000")
+	main_server := http.NewServeMux()
 
-    //Creating Sub Domains
-    server1 := http.NewServeMux()
-    server1.HandleFunc("/", handlerServer1)
+	//Creating Sub Domains
+	server1 := http.NewServeMux()
+	server1.HandleFunc("/", handlerServer1)
 
-    server2 := http.NewServeMux()
-    server2.HandleFunc("/", handlerServer2)
+	server2 := http.NewServeMux()
+	server2.HandleFunc("/", handlerServer2)
 
-    server3 := http.NewServeMux()
-    server3.HandleFunc("/", handlerServer3)
+	server3 := http.NewServeMux()
+	server3.HandleFunc("/", handlerServer3)
 
-    //Running Sub Domains
-    go func() {
-        fmt.Println("Server started on: http://localhost:7777")
-        http.ListenAndServe(":7777", server1)
-    }()
-
-    go func() {
-        fmt.Println("Server started on: http://localhost:8888")
-        http.ListenAndServe(":8888", server2)
-    }()
+	//Running Sub Domains
+	go func() {
+		fmt.Println("Server started on: http://localhost:7777")
+		http.ListenAndServe(":7777", server1)
+	}()
 
 	go func() {
-        fmt.Println("Server started on: http://localhost:9999")
-        http.ListenAndServe(":9999", server2)
-    }()
+		fmt.Println("Server started on: http://localhost:8888")
+		http.ListenAndServe(":8888", server2)
+	}()
 
-    //Running Main Server
-    http.ListenAndServe("localhost:9000", main_server)
+	go func() {
+		fmt.Println("Server started on: http://localhost:9999")
+		http.ListenAndServe(":9999", server2)
+	}()
+
+	//Running Main Server
+	http.ListenAndServe("localhost:9000", main_server)
 }
 
 func handlerServer1(w http.ResponseWriter, r *http.Request) {
-    enrollmentInfo := processQuery(r)
+	enrollmentInfo := processQuery(r)
 
-    fmt.Println(enrollmentInfo.StudentID, enrollmentInfo.ClassNum)
-    fmt.Println("Running on Port :7777")
+	fmt.Println(enrollmentInfo.StudentID, enrollmentInfo.ClassNum)
+	fmt.Println("Running on Port :7777")
 }
 
 func handlerServer2(w http.ResponseWriter, r *http.Request) {
-    enrollmentInfo := processQuery(r)
+	enrollmentInfo := processQuery(r)
 
-    fmt.Println(enrollmentInfo.StudentID, enrollmentInfo.ClassNum)
+	fmt.Println(enrollmentInfo.StudentID, enrollmentInfo.ClassNum)
 	fmt.Println("Running on Port :8888")
 }
 
 func handlerServer3(w http.ResponseWriter, r *http.Request) {
-    enrollmentInfo := processQuery(r)
+	enrollmentInfo := processQuery(r)
 
-    fmt.Println(enrollmentInfo.StudentID, enrollmentInfo.ClassNum)
+	fmt.Println(enrollmentInfo.StudentID, enrollmentInfo.ClassNum)
 	fmt.Println("Running on Port :9999")
+}
+
+func Enroll(servers []*Server, i int, w http.ResponseWriter, r *http.Request) {
+	studentID, err := strconv.Atoi(r.URL.Query().Get("studentID"))
+	if err != nil {
+		log.Panic("something went wrong converting studentID to an int")
+	}
+	classNumber, err := strconv.Atoi(r.URL.Query().Get("classNumber"))
+	if err != nil {
+		log.Panic("something went converting classNumber to an int")
+	}
+	servers[i].Lock()
+	defer servers[i].Unlock()
+	class, found := servers[i].classes[classNumber]
+	if found {
+		fmt.Println("found the class")
+		class.enrollment++
+		success := db.UpdateDB(studentID, classNumber, servers, i)
+		if success {
+			fmt.Fprintf(w, "Successfully enrolled in %v", classNumber)
+		} else {
+			fmt.Fprintf(w, "Sorry, you were not enrolled in class %v", classNumber)
+		}
+	} else {
+		fmt.Fprintf(w, "Sorry, you were not actually enrolled in class %v", classNumber)
+	}
 }
