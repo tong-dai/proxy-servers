32eef18a7485f3bb9626e9f91c58b5f7484c3a6f
Tong <tydai@dynamic-oit-vapornet100-10-9-47-210.princeton.edu>
2023-12-15 12:34:08 -0500

    hi

--- proxy/main.go
+++ proxy/main.go
@@ -32,7 +32,7 @@ func main() {
 	fmt.Println("Server started on: http://localhost:9000")
 	main_server := http.NewServeMux()
 
-	//Creating Sub Domains
+	// Creating Sub Domains
 	server1 := http.NewServeMux()
 	server1.HandleFunc("/", handlerServer1)
 
@@ -42,14 +42,11 @@ func main() {
 	server3 := http.NewServeMux()
 	server3.HandleFunc("/", handlerServer3)
 
-	//Running Sub Domains
+	// Running Sub Domains
 	go func() {
 		fmt.Println("Server started on: http://localhost:7777")
 		http.ListenAndServe(":7777", server1)
 	}()
-
-	fmt.Println(load_balancer.Servers[0].Classes["0"].Enrollment)
-	fmt.Println(load_balancer.Servers[0].Classes["0"].MaxEnrollment)
 
 	// go func() {
 	// 	fmt.Println("Server started on: http://localhost:8888")
@@ -61,44 +58,52 @@ func main() {
 	// 	http.ListenAndServe(":9999", server3)
 	// }()
 
-	//Running Main Server
+	// Running Main Server
 	http.ListenAndServe("localhost:9000", main_server)
 }
 
 func handlerServer1(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Hellor???")
-	// eInfo := processQuery(r)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Content-Type", "text/event-stream")
+
 	eInfo := &EnrollmentInfo{StudentID: r.URL.Query().Get("studentID"), ClassNum: strings.TrimSpace(r.URL.Query().Get("classNum"))}
+	
 	srv := load_balancer.Servers[0]
+	
 	srv.Lock()
 	defer srv.Unlock()
+	
 	fmt.Println(len(srv.Classes))
-	// for key := range srv.Classes {
-	// 	fmt.Printf("key %s", key)
-	// }
-	fmt.Printf("enrollment in class 0: %d\n", srv.Classes["0"].Enrollment)
-	fmt.Printf("classNum: %s\n", eInfo.ClassNum)
-	class, found := srv.Classes[eInfo.ClassNum]
-	if !found {
-		msg := "you cannot enroll in class 0"
-		fmt.Println(msg)
-		w.Write([]byte(msg))
-		return
-	}
-	class.Enrollment++
-	fmt.Printf("new enrollment %d\n", class.Enrollment)
-	success := database.UpdateDB(load_balancer, eInfo.StudentID, eInfo.ClassNum, 0)
-	if success {
-		fmt.Println("successful update of the database")
-	} else {
-		msg := "you were not enrolled in class " + eInfo.ClassNum
-		w.Write([]byte(msg))
-	}
-	fmt.Println(eInfo.StudentID, eInfo.ClassNum)
 
-	fmt.Println("Running on Port :7777")
-	message := "byE BYE BYE"
-	w.Write([]byte(message))
+
+	class, found := srv.Classes[eInfo.ClassNum]
+	for {
+		if !found {
+			msg := "you cannot enroll in class 0"
+			fmt.Println(msg)
+			w.(http.Flusher).Flush()
+			return
+		}
+	}
+	
+
+
+	// class.Enrollment++
+	// fmt.Printf("new enrollment %d\n", class.Enrollment)
+	// success := database.UpdateDB(load_balancer, eInfo.StudentID, eInfo.ClassNum, 0)
+	// if success {
+	// 	fmt.Println("successful update of the database")
+	// } else {
+	// 	msg := "you were not enrolled in class " + eInfo.ClassNum
+	// 	w.Write([]byte(msg))
+	// }
+	// fmt.Println(eInfo.StudentID, eInfo.ClassNum)
+
+	// fmt.Println("Running on Port :7777")
+	// message := "byE BYE BYE"
+	// w.Write([]byte(message))
 }
 
 func handlerServer2(w http.ResponseWriter, r *http.Request) {
