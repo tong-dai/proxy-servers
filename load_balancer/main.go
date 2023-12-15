e3d99d0b571e6551cf9b54fde1d7ae872b3bf91f
Tong <tydai@dynamic-oit-vapornet100-10-9-47-210.princeton.edu>
2023-12-14 20:21:40 -0500

    made lb into a package

--- load_balancer/main.go
+++ load_balancer/main.go
@@ -3,53 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"sync"
+	"cos316/td_ec_final_project/package"
 )
 
-type LB struct {
-	Servers		[]*Server
-	Current		int
-	mu			sync.Mutex
-}
 
-type Server struct {
-	ServerURL	string
-}
-
-func (lb *LB) getNextServer() *Server {
-	lb.mu.Lock()
-	defer lb.mu.Unlock()
-
-	lb.Current = (lb.Current + 1) % len(lb.Servers)
-    return lb.Servers[lb.Current]
-}
-
-func (lb *LB) handleRequest(w http.ResponseWriter, r *http.Request) {
-
-	server := lb.getNextServer()
-
-	serverURL, err := url.Parse(server.ServerURL)
-	if err != nil {
-		fmt.Println("ERROR")
-		return
-	}
-
-	proxy := httputil.NewSingleHostReverseProxy(serverURL)
-	proxy.ServeHTTP(w, r)
-}
 
 func main() {
-	lb := LB{
-        Servers: []*Server{
+	lb := load_balancer.LB{
+        Servers: []*load_balancer.Server{
             {ServerURL: "http://localhost:7777"},
             {ServerURL: "http://localhost:8888"},
-			// {ServerURL: "http://localhost:9999"},
+			{ServerURL: "http://localhost:9999"},
         },
     }
 
-    http.HandleFunc("/", lb.handleRequest) 
+    http.HandleFunc("/", lb.HandleRequest) 
     fmt.Println("Server is running on port 8080...")
     http.ListenAndServe(":8080", nil)
 }
